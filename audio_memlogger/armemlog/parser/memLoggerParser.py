2e9ed505b8dce9c95da6ba38bbdf4f56755c7dd4
Radhika Garg <radhgarg@qti.qualcomm.com>
2026-01-12 23:30:11 -0800

    Update copyright and license marking.
    - Replaced "Qualcomm Innovation Center, Inc." with
      "Qualcomm Technologies, Inc. and/or its subsidiaries"
      across multiple source files.
    - Update SPDX identifier from BSD-3-Clause-Clear to BSD-3-Clause
    
    Signed-off-by: ffrancis <ffrancis@qti.qualcomm.com>

--- audio_memlogger/armemlog/parser/memLoggerParser.py
+++ audio_memlogger/armemlog/parser/memLoggerParser.py
@@ -1,6 +1,6 @@
 #    Main Parser file for mem logger
-#    Copyright (c) Qualcomm Innovation Center, Inc. All rights reserved.
-#    SPDX-License-Identifier: BSD-3-Clause-Clear
+#    Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+#    SPDX-License-Identifier: BSD-3-Clause
 import sys
 import os
 import memLoggerUtils
